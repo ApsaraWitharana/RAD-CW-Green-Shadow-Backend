b9f04e6c7d90f29de7b672c49d29d94a75e5bf91
ApsaraWitharana <asachini095@gmail.com>
2025-01-23 21:59:55 +0530

    add-staff add,update,delete getall function impl

--- controller/StaffController.ts
+++ controller/StaffController.ts
@@ -6,7 +6,7 @@ const prisma = new PrismaClient();
 
 export async function addStaff(staff:Staff){
     try {
-        await prisma.staff.create({
+       const newStaff = await prisma.staff.create({
             data:{
                 firstName:staff.firstName,
                 lastName:staff.lastName,
@@ -24,21 +24,62 @@ export async function addStaff(staff:Staff){
                 role:staff.role
             }
         })
-        console.log('staff added successfully!!')
+        console.log('staff added successfully!!',newStaff)
+        return newStaff;
     }catch (err){
-        console.log(err)
+        console.log("error adding staff",err);
     }
 }
 
 export async function deleteStaff(id:number){
     try {
-        await prisma.staff.delete({
+      const deleteStaff =  await prisma.staff.delete({
             where:{
                 id:id,
             }
         })
-        console.log('Staff delete Successfully!!');
+        console.log('Staff delete Successfully!!',id);
+       return deleteStaff;
     }catch (err){
-        console.log(err);
+        console.log("delete staff ",err);
+    }
+}
+
+export async function updateStaff(id:number,staff:Staff){
+    try {
+     const updateStaff =   await prisma.staff.update({
+            where:{
+                id:id,
+            },
+            data:{
+                firstName:staff.firstName,
+                lastName:staff.lastName,
+                designation:staff.designation,
+                gender:staff.gender,
+                joinedDate: staff.joinedDate,
+                dob:staff.dob,
+                addressLine1:staff.addressLine1,
+                addressLine2:staff.addressLine2,
+                addressLine3:staff.addressLine3,
+                addressLine4:staff.addressLine4,
+                addressLine5:staff.addressLine5,
+                contactNumber:staff.contactNumber,
+                email:staff.email,
+                role:staff.role,
+            }
+        })
+        console.log('staff update successfully!!',updateStaff)
+        return updateStaff;
+    }catch (err){
+        console.log("error update staff ",err)
+    }
+}
+
+export async function getStaffs(){
+    try {
+        return await prisma.staff.findMany();
+
+    }catch (err){
+        console.log("error getting staff from prisma data ",err);
     }
 }
--- routes/Staff-routes.ts
+++ routes/Staff-routes.ts
@@ -1,11 +1,12 @@
 import express from "express";
-import {addStaff, deleteStaff} from "../controller/StaffController";
+import {addStaff, deleteStaff, getStaffs, updateStaff} from "../controller/StaffController";
 const router = express.Router();
 
 router.post("/add", async (req, res) => {
     const newStaff = req.body;
     try {
-        await addStaff(newStaff);
+        const addedStaff = await addStaff(newStaff);
+        res.json(addedStaff);
         res.send('staff added');
     } catch (err) {
         console.log(err);
@@ -13,12 +14,42 @@ router.post("/add", async (req, res) => {
 });
 
 // delete
-router.delete("/delete/:id",(req, res) => {
+router.delete("/delete/:id",async (req, res) => {
 
     const id = +req.params.id;
-    deleteStaff(id);
-    res.send('staff deleted')
+    try {
+        const deletedStaff = await deleteStaff(id);
+        res.json(deletedStaff);
+        res.send('staff deleted');
+    }catch (err){
+        console.log("error delete staff",err);
+    }
 });
 
+//update
+router.put('/update/:id',async (req, res) =>{
+
+    const id = +req.params.id;
+    const staff = req.body;
+    try {
+        const updatedStaff = await updateStaff(id,staff);
+        res.json(updatedStaff);
+        res.send('staff updated')
+    }catch (err){
+        console.log("error update staff",err)
+    }
+
+});
+
+//getAll
+router.get('/get',async (req, res) =>{
+    try{
+        const staffs = await getStaffs();
+        res.json(staffs);
+        res.send('get all staff');
+    }catch (err){
+        console.log('error getting staffs',err);
+    }
+})
 
 export default router;
